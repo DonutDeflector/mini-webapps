19542fe578e9c5ab3675ed148fc7bc42a41d02e0
DonutDeflector <donutdeflector@protonmail.com>
2017-11-03 22:59:41 -0400

    further work on liszt

--- liszt/assets/js/liszt.js
+++ liszt/assets/js/liszt.js
@@ -1,23 +1,47 @@
 $(document).ready(function() {
 
   // Global Variables
-  let item = null;
+  var numberList = 0;
+  var numberCompleted = 0;
+
+  console.log(numberList);
+  console.log(numberCompleted);
+
+  // Add List Heading
+  var addListHeading = function() {
+    if (numberList === 0) {
+      $("#list").append("<h2>List</h2>");
+      numberList = numberList++
+      return numberList;
+    } else {
+      numberList = numberList++;
+      return numberList;
+    }
+  }
 
   // Add item
-  addItem = function () {
+  var addItem = function() {
     if (document.getElementById("new-item").value !== "") {
       let item = document.getElementById("new-item").value;
-      $("#list").append("<div id='list-item'>" + "<span>" + item + "</span>" + "<i class='fa fa-check-circle-o' aria-hidden='true'></i>" + "</div>");
+      $("#list").append("<div id='list-item'>" + "<p>" + item + "<div id='item-actions'><button id='done'>Done</button><button id='delete'>Delete</button></div>" + "</div>");
 
       item = null;
       document.getElementById("new-item").value = null;
     }
-  }
+  };
 
   // Run Functions
   $("#new-item").keydown(function(e) {
     if (e.which == 13) {
+      addListHeading();
       addItem();
+    }
+  });
+
+  // Done and Delete Actions
+  $("#done").click(function() {
+    if (numberCompleted === 0) {
+      $('container').append("<div id='completed'><h2>Completed</h2></div>")
     }
   });
 });
