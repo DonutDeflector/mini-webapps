975e67a527ea865e119a00e104dd8abb0b736145
DonutDeflector <donutdeflector@tuta.io>
2017-11-25 15:47:06 +0000

    finished Moolah

---
+++ moolah/README.md
@@ -0,0 +1,9 @@
+# Moolah
+
+Quick currency converter. Marks my first time using an API.
+
+## Web Technologies Used
+
+1. [fixer.io](http://fixer.io/) - Exchange rate and currency conversion API
+2. [money.js](https://openexchangerates.github.io/money.js/) - Javascript currency conversion library
+3. [jQuery](https://jquery.com/) - Multipurpose Javascript library
--- moolah/assets/js/moolah.js
+++ moolah/assets/js/moolah.js
@@ -16,13 +16,41 @@ $.getJSON(
   }
 );
 
-let foo = null;
-
-function bar() {
-  foo = document.getElementById("base").value;  
-  return foo;
+// Obtain Values of Base, Rate, and their amounts
+function obtainValues() {
+  baseAmount = document.getElementById("base-amount").value;
+  rateAmount = document.getElementById("rate-amount").value;
+  base = document.getElementById("base").value;
+  rate = document.getElementById("rate").value;
+  
+  return baseAmount;
+  return rateAmount;
+  return base;
+  return rate;
 }
 
-$("#submit").click(function(e) {
-  bar();
+// Convert Base to Rate
+function convertBase() {
+  document.getElementById('rate-amount').value = fx.convert(baseAmount, {from: base, to: rate}).toFixed(2);
+}
+
+// Convert Rate to Base
+function convertRate() {
+  document.getElementById('base-amount').value = fx.convert(rateAmount, {from: rate, to: base}).toFixed(2);
+}
+
+// Run functions on enter for Base
+$('#base-amount').keydown(function(e) {
+  if (e.which == 13) {
+    obtainValues();
+    convertBase();
+  }
+});
+
+// Run functions on enter for Rate
+$('#rate-amount').keydown(function(e) {
+  if (e.which == 13) {
+    obtainValues();
+    convertRate();
+  }
 });
